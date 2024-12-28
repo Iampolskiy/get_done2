8b2dfa8439e60be581b1e5b699a929455b73d0e1
Alexandr <114815595+Iampolskiy@users.noreply.github.com>
2024-12-28 07:45:47 +0100

    delete geht security ok

--- app/allmychallenges/[id]/page.tsx
+++ app/allmychallenges/[id]/page.tsx
@@ -1,12 +1,24 @@
 import { Challenge } from "@/types/types";
 import { PrismaClient } from "@prisma/client";
 import MyChallengeClient from "./MyChallengeClient";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function ChallengesPage({
   params,
 }: {
   params: { id: string };
 }) {
+  const userNow = await currentUser();
+  const userNowEmail = userNow?.emailAddresses?.[0]?.emailAddress;
+
+  // Überprüfe, ob der Benutzer authentifiziert ist und eine E-Mail-Adresse hat
+  if (!userNow || !userNowEmail) {
+    return (
+      <div>
+        <p>Benutzer ist nicht authentifiziert oder E-Mail-Adresse fehlt.</p>
+      </div>
+    );
+  }
   const { id } = await params;
   const prisma = new PrismaClient();
   const numericId = parseInt(id, 10);
