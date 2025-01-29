8df833d897ddbb314a2fa35bcdff4aed853b06ae
Kempalshik <alexandr.iampolskaia@gmail.com>
2025-01-29 22:21:06 +0100

    wieder zurück zum anfang.

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    /* domains: ["gateway.pinata.cloud"], */
     remotePatterns: [
       {
         protocol: "https",
         hostname: "sapphire-obliged-puma-478.mypinata.cloud",
         port: "",
         pathname: "/files/**",
+      },
+      {
+        protocol: "https",
+        hostname: "gateway.pinata.cloud",
+        port: "",
+        pathname: "/ipfs/**",
       },
     ],
   },
