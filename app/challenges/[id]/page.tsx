8c8496fefbbace3e71833f5af62bcf7432a16de9
alexandr_new <alexandr.iampolskaia@gmail.com>
2025-05-15 14:08:50 +0200

    await weg in create test

--- app/challenges/[id]/page.tsx
+++ app/challenges/[id]/page.tsx
@@ -41,7 +41,7 @@ export default async function ChallengePage({
 }: {
   params: { id: string };
 }) {
-  const { id } = await params;
+  const { id } = /* await */ params;
   const numericId = parseInt(id, 10);
   // Direkt die Datenbank abfragen
   const challenge = await prisma.challenge.findUnique({
